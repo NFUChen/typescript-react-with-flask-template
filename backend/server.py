1878a75440944056e10462076cb76bb9013b873f
William Chen <wichen@sram.com>
2023-07-14 16:13:10 +0800

    change default docker-compose port

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,9 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
-PORT = os.environ["PORT"]
 
 @app.route("/index")
 def index():
@@ -15,4 +12,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug= False, port= PORT)
+    app.run(host="0.0.0.0", debug= False, port= 8080)
